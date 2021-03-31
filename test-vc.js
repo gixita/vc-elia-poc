fd6a6e5cd2f4a28a298ac076b0cbb78e459e3c34
gixita <kris.laermans@gmail.com>
2021-03-31 11:29:44 +0200

    add vc-cli

--- test-vc.js
+++ test-vc.js
@@ -1,103 +1,27 @@
 const vc = require('vc-js');
 const axios = require('axios');
+const keyfile = require('./my-key.json');
 
-const {Ed25519KeyPair, suites: {Ed25519Signature2018}} = require('jsonld-signatures');
+
 const Secp256k1KeyPair = require('secp256k1-key-pair');
 const EcdsaSepc256k1Signature2019 = require('ecdsa-secp256k1-signature-2019');
-
-var controllerDoc;
-var publicKeyDoc;
-// Sample unsigned credential
-
-
-class KMSInteraction {
-  constructor({timeout = 5000}) {
-    this.axiosInstance = axios.create({
-      baseURL: 'http://localhost:3000',
-      headers: {Authorization: `token none`},
-      timeout,
-    });
-    this.filename = null;
-    this.url = null;
-    this.fullRawUrl = null;
-  }
-  
-
-  async create({filename, content}) {
-    let result;
-    try {
-      result = await this.axiosInstance.post('/publickeydoc', {
-        public: false,
-        files: {
-          [filename]: {
-            content: JSON.stringify(content, null, 2)
-          }
-        }
-      });
-    } catch(e) {
-      throw new Error('Error creating gist', e);
-    }
-    this.filename = filename;
-    this.url = "http://localhost:3000/publickeydoc";
-    // We need to adapt the full raw url
-    this.fullRawUrl = "http://localhost:3000/publickeydoc/"+filename;
-  }
-
-  async update({content}) {
-    if(!this.filename) {
-      throw new Error('Gist has not been created.');
-    }
-    let result;
-    try {
-      result = await this.axiosInstance.post(this.url, {
-        public: false,
-        files: {
-          [this.filename]: {
-            content: JSON.stringify(content, null, 2)
-          }
-        }
-      });
-    } catch(e) {
-      throw new Error('Error updating gist', e);
-    }
-    this.fullRawUrl = "http://localhost:3000/publickeydoc/";//+filename;
-  }
-}
+const didContext = require('did-context');
+const {Ed25519KeyPair, suites: {Ed25519Signature2018}, extendContextLoader} = require('jsonld-signatures');
 
 async function asyncfunc(){
-
-    const k = await Secp256k1KeyPair.generate();
-    k.id = 'https://example.edu/issuers/keys/1'; // See Key ID section
-    k.controller = 'https://example.com/i/carol'; // See Controller Document section
     
-    const suite = new EcdsaSepc256k1Signature2019({
+    
+    k = await Ed25519KeyPair.from(keyfile)
+
+    // console.log(k)
+
+    
+    const suite = new Ed25519Signature2018({
       verificationMethod: k.id,
       key: k
     });
 
-    const controllerStore = new KMSInteraction({});
-    const keyStore = new KMSInteraction({});
-
-    await Promise.all([
-      controllerStore.create({filename: 'creator.json', content: {}}),
-      keyStore.create({filename: 'key.json', content: {}}),
-    ]);
-
-    const publicKeyDoc = Object.assign(k.publicNode(), {
-      '@context': 'https://w3id.org/security/v2',
-      id: "http://localhost/publickeydoc",
-      controller: "http://localhost/controllerdoc"
-    });
-    const controllerDoc = {
-      '@context': 'https://w3id.org/security/v2',
-      id: "http://localhost/controllerdoc",
-      assertionMethod: ["http://localhost/publickeydoc"]
-    };
-    // update controller and key documents
-    await Promise.all([
-      controllerStore.update({content: controllerDoc}),
-      keyStore.update({content: publicKeyDoc}),
-    ]);
+    
     // Sample unsigned credential
     const credential = {
       "@context": [
@@ -115,41 +39,66 @@ async function asyncfunc(){
     };
 
 
-    console.log("---- starting ---------");
+    // console.log("---- starting ---------");
  
 
 
     
     const vc1 = await vc.issue({credential, suite});
-    // console.log(JSON.stringify(vc1, null, 2));
+    console.log(JSON.stringify(vc1, null, 2));
     
     
     const verifiableCredential = vc1; // either array or single object
 
-    // optional `id` and `holder`
+    // // optional `id` and `holder`
     const id = 'ebc6f1c2';
-    const holder = 'did:ex:12345';
+    const holder = keyfile.controller;
     
-    const presentation = vc.createPresentation({
+    var presentation = vc.createPresentation({
       verifiableCredential, id, holder
     });
     
-    // console.log(JSON.stringify(presentation, null, 2));
+    console.log(JSON.stringify(presentation, null, 2));
 
 
 
-    // const challenge = 'mychallengegivenbytheportal'
-    // const vp = await vc.signPresentation({
-    //     presentation, suite, challenge
-    //   });
-      
+    const challenge = 'mychallengegivenbytheportal'
+    const vp = await vc.signPresentation({
+        presentation, suite, challenge
+      });
+
+    presentation = vp
+    
+    const documentLoader = extendContextLoader(async url => {
+        if(url.startsWith('did:key:')) {
+          return {
+            contextUrl: null,
+            url,
+            document: await get({url})
+          };
+        }
+        const context = didContext.contexts.get(url);
+        if(context) {
+          return {
+            contextUrl: null,
+            url,
+            document: context
+          };
+        }
+        return vc.defaultDocumentLoader(url);
+      });
     // console.log(JSON.stringify(vp, null, 2));
 
+    const result = await vc.verify({
+        presentation, 
+        challenge, 
+        suite: [new Ed25519Signature2018(), new EcdsaSepc256k1Signature2019()],
+        documentLoader
+    });
 
-
-    const result = await vc.verify({presentation, suite, unsignedPresentation: true});
-    // console.log('---- Verify -------');
-    // console.log(result);
+    // const result = await vc.verify({presentation, suite, unsignedPresentation: true});
+    console.log('---- Verify -------');
+    console.log(result);
 
 
 }
