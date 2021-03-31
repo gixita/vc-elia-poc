b91e427dccdfc67c634c4929a50701fcddd1f01c
Simon Boigelot <sboigelot@gmail.com>
2021-03-31 14:10:33 +0200

    Link to claim management

--- server_modules/claim_management.js
+++ server_modules/claim_management.js
@@ -11,5 +11,9 @@ module.exports = class ClaimManagement
       app.get('/' + this.base_uri + '/index', (req, res) => {
             res.render(this.view_folder + '/index')
         });
+        
+      app.get('/' + this.base_uri + '/qrcode', (req, res) => {
+        res.render(this.view_folder + '/index')
+    });
     }
 };
