7225d3a126eb91000bb93f3849b25ffbf336fdfe
Simon Boigelot <sboigelot@gmail.com>
2021-04-01 15:38:07 +0200

    portal redirection on claim validated

--- public/Login V1_files/check_claim_management.js
+++ public/Login V1_files/check_claim_management.js
@@ -2,11 +2,20 @@
 const check_claim = function()
 { 
     console.log("checking claim")
-    $.getJSON('claim_management_verify', function(data) {
+    $.getJSON('get_notification_verification_is_available', function(data) {
     // JSON result in `data` variable
+
+    if(data)
+    {
+        console.log("verfication is valid")
+        window.location.replace("../portal/verified");
+    }
+    else
+    {
+        console.log("waiting on verification")
+    }
 });
 }
 
 setInterval(check_claim, 5000);
-
 console.log("check_claim_management initialized")
